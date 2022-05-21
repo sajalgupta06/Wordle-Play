9fa8bce4be8e6e7b9ec39e96fd49e0df740d7cd5
Sajal Gupta <guptasajal6@gmail.com>
2022-05-21 11:54:04 +0530

    removed heading

--- src/App.js
+++ src/App.js
@@ -217,7 +217,6 @@ const App = () => {
       className="h-100 text-center  md-3 pt-lg-4 px-0 px-sm-4 topDiv"
     >
       <header className="d-sm-none d-md-block">
-        <h5 className="mt-md-4 mt-lg-5">CodeFoster Presents</h5>
         <h1>Wordle <span style={{color:'#ff7f50'}}>Play</span></h1>
       </header>
       <div className="mainDiv">
